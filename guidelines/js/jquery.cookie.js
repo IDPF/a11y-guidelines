518b051f66ade535b67fff0210a01fcd95d4c206
Matt Garrish <matt.garrish@gmail.com>
2016-08-05 14:20:35 -0400

    converted dynamic php pages to static html

--- guidelines/js/jquery.cookie.js
+++ guidelines/js/jquery.cookie.js
@@ -4,7 +4,7 @@
  *
  * Copyright 2011, Klaus Hartl
  * Dual licensed under the MIT or GPL Version 2 licenses.
- * http://www.opensource.org/licenses/mit-license.php
+ * http://www.opensource.org/licenses/mit-license.html
  * http://www.opensource.org/licenses/GPL-2.0
  */
 (function($) {
